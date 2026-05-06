be71053a75c12492f4d9181262596df804d83abb
Lor Vengroth <lorvengrothofficial@gmail.com>
2026-05-06 13:39:28 +0700

    Update MovieService.java

--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -12,15 +12,15 @@ import java.util.List;
 
 public interface MovieService {
 
-    // Search movies by query with pagination
+
     SearchResponse searchMovies(String query, int page);
 
-    // Get detailed information for a specific movie
+
     MovieDetail getMovieDetails(int movieId);
 
-    // Get all videos (trailers, clips, etc.) for a movie
+ 
     List<Video> getMovieVideos(int movieId);
 
-    // Get the official trailer URL for a movie
+ 
     String getTrailerUrl(int movieId);
 }
